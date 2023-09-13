90543611b6678a036b52f69302ee253ff0bd25d5
Navjot kumar <110272866+navjot-kumar-15@users.noreply.github.com>
2023-09-13 15:52:07 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -86,7 +86,7 @@ const Login = () => {
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                {isLoading && !user ? <Loader2 /> : " Sign in"}
+                {isLoading ? <Loader2 /> : " Sign in"}
               </button>
             </div>
           </form>
