377ddd18ae1692cd35978763675512774f180881
navjot <15navjotverma@gmail.com>
2023-09-13 21:11:13 +0530

    sign out Updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,19 +6,6 @@ import { useDispatch } from "react-redux";
 import { logout } from "../features/auth/authSlice";
 import { useNavigate } from "react-router-dom";
 
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-  { name: "Reports", href: "#", current: false },
-];
 const userNavigation = [{ name: "Sign out" }];
 
 function classNames(...classes) {
@@ -67,13 +54,14 @@ const Navbar = ({ children }) => {
                           leaveTo="transform opacity-0 scale-95"
                         >
                           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <span className="pl-5">{userInfo.email}</span>
                             {userNavigation.map((item, index) => (
                               <Menu.Item key={item.name}>
                                 {({ active }) => (
                                   <a
                                     className={classNames(
                                       active ? "bg-gray-100" : "",
-                                      "block px-4 py-2 text-sm text-gray-700"
+                                      "block px-4 py-2 text-sm  cursor-pointer bg-green-700 mt-3 text-white rounded-md m-1 hover:bg-green-600 transition-all ease-in-out"
                                     )}
                                     onClick={() => {
                                       dispatch(logout());
